f9e7cc916b787f61bec6f11a492778ef26d0ff18
Manoj Sethi <mjsh@gft.com>
2022-12-06 11:38:15 +0000

    day 6

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import day2 from './day2';
 import day3 from './day3';
 import day4 from './day4';
 import day5 from './day5';
-day5();
+import day6 from './day6';
+day6();
 // import fs from 'fs';
 // import path from 'path';
 
